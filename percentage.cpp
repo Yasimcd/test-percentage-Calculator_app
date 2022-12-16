2171b3b6d4dac532939212a3ca0b73666bae6009
Yasim <91527193+Yasimcd@users.noreply.github.com>
2022-12-16 14:39:19 -0800

    Add files via upload

---
+++ percentage.cpp
@@ -0,0 +1,31 @@
+// Lab 3 percentage.cpp
+// This program will determine the percentage 
+// of answers a student got correct on a test.
+// Yasim Cruz
+
+#include <iostream> 
+#include <iomanip>    
+#include <string>   
+using namespace std;
+
+int main()
+{
+   string name;
+   int numQuestions,numCorrect;
+   double percentage;
+   
+   // Get student's test data
+   cout << "Enter student's first and last name: ";
+   getline(cin,name);
+   
+   cout << "Number of questions on the test: ";
+   cin  >> numQuestions;
+   cout << "Number of answers the student got correct: ";
+   cin  >> numCorrect;
+   
+   // Compute and display the student's % correct
+   percentage = static_cast<double>(numCorrect)/numQuestions * 100;
+   cout << name << setw(8) << fixed << setprecision(1) << percentage;
+   
+   return 0;
+}
